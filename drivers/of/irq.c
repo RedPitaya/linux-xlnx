bb418dccfd16ec82671e16dd7ec0f333540abea1
n.danilyuk <n.danilyuk@integrasources.com>
2022-08-24 13:38:02 +0700

    T3

--- drivers/of/irq.c
+++ drivers/of/irq.c
@@ -30,7 +30,15 @@
  * Added by DM
 **/
 extern int xilinx_intc_of_init_done;
-extern int xilinx_intc_of_init(struct device_node *, struct device_node *);
+
+#ifndef CONFIG_IRQCHIP_XILINX_INTC_MODULE_SUPPORT_EXPERIMENTAL
+extern int __init xilinx_intc_of_init(struct device_node *intc,
+					     struct device_node *parent);
+#else
+extern int xilinx_intc_of_init(struct device_node *intc,
+			       struct device_node *parent);
+#endif
+
 unsigned int _irq_of_parse_and_map(struct device_node *dev, int index)
 {
 	struct of_phandle_args oirq;
